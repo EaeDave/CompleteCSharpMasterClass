4b171d1725a90d43d0aa6139acfab0959d495288
EaeDave <eaedav3@gmail.com>
2024-12-09 13:00:39 -0300

    Using variables - String

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,3 +2,20 @@
 Console.WriteLine("Hello, World!");
 Console.ReadKey();
 // "This is a comment, and it won't be executed."
+
+// declare a (string) variable
+string myFriendsName;
+
+// assign a value into the variable
+myFriendsName = "Caio";
+
+// use/access the variable
+Console.WriteLine(myFriendsName);
+
+// variables can be reassigned and overwriten
+myFriendsName = "Heitor";
+Console.WriteLine(myFriendsName);
+
+// variables can be declared and initialized at the same time
+string myCatsName = "Tommy";
+Console.WriteLine(myCatsName);
