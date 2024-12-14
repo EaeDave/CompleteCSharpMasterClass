3330028b4ec0371a15cb0b30e1ddafa4b50149a6
EaeDave <EaeDav3@gmail.com>
2024-12-14 16:55:37 -0300

    Overview of Datatypes

---
+++ DataTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿// Referency type
+string myName = "David";
+
+// Value types
+int myAge = 23;
+double myHeight = 1.70;  // 8 bytes (64bits)
+
+// Byte type can be assigned with a number from zero (0) to two hundreds and fifth five (255)
+byte age = 25;  // 1 byte (8bits)
+
+// Signed byte means that number can be negative (-), minimum -128 and max 127
+sbyte newAge = -128;
+
+short linkdinConnections = 32550;  // 2 bytes (16bits)
+
+long phoneNumber = 017657723991;
+
+float pi = 3.14f;  // 4 bytes (32bits)
+
+// https://learn.microsoft.com/pt-br/dotnet/csharp/language-reference/builtin-types/built-in-types
